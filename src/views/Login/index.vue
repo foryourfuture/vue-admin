becc755f36a9df04a1d7687f689676140c23c183
foryourfuture <foryourfuture@gmail.com>
2020-04-22 22:23:49 +0800

    构建项目，配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login">
+    这里是登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  props: {},
+  watch: {}
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
